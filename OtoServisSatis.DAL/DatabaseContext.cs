8f0d217621c11cef06d161adb895ec233ee91a2d
ozgrfdn@gmail.com <ozgrfdn@gmail.com>
2023-03-30 21:08:35 +0300

    Calismak

--- OtoServisSatis.DAL/DatabaseContext.cs
+++ OtoServisSatis.DAL/DatabaseContext.cs
@@ -1,7 +1,52 @@
-﻿namespace OtoServisSatis.DAL
-{
-    public class DatabaseContext
-    {
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using OtoServisSatis.Entities;
 
+namespace OtoServisSatis.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Arac> Arac { get; set; }
+        public DbSet<Kullanici> Kullanicilar { get; set; }
+        public DbSet<Marka> Markalar { get; set; }
+        public DbSet<Musteri> Musteriler { get; set; }
+        public DbSet<Rol> Roller { get; set; }
+        public DbSet<Satis> Satislar { get; set; }
+        public DbSet<Servis> Servisler { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public class DatabaseInitializer : CreateDatabaseIfNotExists<DatabaseContext>
+        {
+            protected override void Seed(DatabaseContext context)
+            {
+                //if (!context.Kullanicilar.Any())
+                //{
+                //    context.Kullanicilar.Add(new Entities.Kullanici()
+                //    {
+                //        Aktifmi = true,
+                //        Adi = "Admin",
+                //        EklenmeTarihi = DateTime.Now,
+                //        Email = "admin@otoservis.com",
+                //        KullaniciAdi = "admin",
+                //        Sifre = "123456"
+                //    });
+                //    context.SaveChanges();
+                //}
+                base.Seed(context);
+            }
+        }
+
+        public DatabaseContext():base("DatabaseContext")
+        {
+            Database.SetInitializer(new DatabaseInitializer());
+        }
     }
+
 }
---
+++ OtoServisSatis.DAL/Migrations/Configuration.cs
@@ -0,0 +1,41 @@
+﻿namespace OtoServisSatis.DAL.Migrations
+{
+    using OtoServisSatis.Entities;
+    using System;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OtoServisSatis.DAL.DatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(OtoServisSatis.DAL.DatabaseContext context)
+        {
+            if (!context.Kullanicilar.Any())
+            {
+                var rol = context.Roller.Add(new Rol()
+                {
+                    Adi = "Admin"
+                });
+
+                context.Kullanicilar.Add(new Kullanici()
+                {
+                    Aktifmi = true,
+                    Adi = "Admin",
+                    EklenmeTarihi = DateTime.Now,
+                    Email = "admin@otoservis.com",
+                    KullaniciAdi = "admin",
+                    Sifre = "123456",
+                    Rol = rol,
+                    RolId = rol.Id
+                });
+                context.SaveChanges();
+            }
+
+            base.Seed(context);
+        }
+    }
+}
