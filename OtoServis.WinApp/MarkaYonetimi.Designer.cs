e3376414da3e1e0734e5d703f47a671728109467
ozgrfdn@gmail.com <ozgrfdn@gmail.com>
2023-04-01 11:38:49 +0300

    Marka Yönetim Formu

---
+++ OtoServis.WinApp/MarkaYonetimi.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace OtoServis.WinApp
+{
+    partial class MarkaYonetimi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMarkalar = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMarkalar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMarkalar
+            // 
+            this.dgvMarkalar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMarkalar.Location = new System.Drawing.Point(13, 13);
+            this.dgvMarkalar.Name = "dgvMarkalar";
+            this.dgvMarkalar.RowHeadersWidth = 51;
+            this.dgvMarkalar.RowTemplate.Height = 24;
+            this.dgvMarkalar.Size = new System.Drawing.Size(399, 425);
+            this.dgvMarkalar.TabIndex = 0;
+            this.dgvMarkalar.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMarkalar_CellClick);
+            // 
+            // MarkaYonetimi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvMarkalar);
+            this.Name = "MarkaYonetimi";
+            this.Text = "MarkaYonetimi";
+            this.Load += new System.EventHandler(this.MarkaYonetimi_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMarkalar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMarkalar;
+    }
+}
---
+++ OtoServis.WinApp/MarkaYonetimi.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Forms;
+using OtoServisSati.BL;
+
+namespace OtoServis.WinApp
+{
+    public partial class MarkaYonetimi : Form
+    {
+
+        MarkaManager manager = new MarkaManager();
+
+        void Yukle()
+        {
+            dgvMarkalar.DataSource = manager.GetAll();
+        }
+        public MarkaYonetimi()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvMarkalar_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void MarkaYonetimi_Load(object sender, System.EventArgs e)
+        {
+            Yukle();
+        }
+    }
+}
